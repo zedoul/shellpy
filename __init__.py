007aabfa8d3280ec8106b124105d78eef3e4958e
shyeon.kim <shyeon.kim@scipi.net>
2013-05-30 20:31:03 +0900

    Fixing __init__.py
    Signed-off-by: shyeon.kim <shyeon.kim@scipi.net>

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
-# markovy
+# shellpy
 # Copyright 2013 Seonggyun Kim
 # See LICENSE for details.
 
-"""
-Python Markov chain library
-"""
-__version__ = '0.12'
+__version__ = '0.0.2'
 __author__ = 'Seonghyun Kim'
 __license__ = 'MIT'
 
-from markovy.markovchains import MarkovChains
+from shellpy.shell import ShellPy
 
